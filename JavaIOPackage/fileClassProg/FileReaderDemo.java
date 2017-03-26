48d2cacb05125c5d592bfd92fbd73c5b580eb249
Aarish Saxena <aarish.work@gmail.com>
2017-03-26 16:19:07 +0530

    Some Changes

---
+++ JavaIOPackage/fileClassProg/FileReaderDemo.java
@@ -0,0 +1,12 @@
+package fileClassProg;
+
+public class FileReaderDemo
+{
+
+    public static void main(String[] args)
+    {
+	// TODO Auto-generated method stub
+
+    }
+
+}
--- JavaIOPackage/fileClassProg/FileWriterDemo.java
+++ JavaIOPackage/fileClassProg/FileWriterDemo.java
@@ -16,10 +16,23 @@ public class FileWriterDemo
 	File f = new File(path,"abc.txt");
 //	Constructor 2)
 	FileWriter fw1 = new FileWriter(f);
-	String str = "Hello";
-	fw1.write(str);
-	
+//	String str = "Hello\n";
+//	fw1.write(str);
 	fw1.close();
+//	Above 2 constructors will override existing data present in file
+	
+//	Below 2 constructors will append existing data present in file
+	FileWriter fw2 = new FileWriter("abc.txt", true);
+	
+	FileWriter fw3 = new FileWriter(f,true);	// to append instead of overwrite
+	fw3.write("Hi");
+	fw3.write("\r\n");
+	fw3.write("nice");
+	fw3.write("\r Meeting");
+	
+	fw3.flush();
+	fw3.close();
+	
     }
 
 }
