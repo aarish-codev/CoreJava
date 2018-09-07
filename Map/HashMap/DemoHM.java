a346f3b9bee6ed3f32e8a0221e090f26236d2a71
Aarish Saxena <aarish.work@gmail.com>
2018-09-07 14:20:43 +0530

    modified DemoHM

--- Map/HashMap/DemoHM.java
+++ Map/HashMap/DemoHM.java
@@ -1,7 +1,10 @@
 package HashMap;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class DemoHM
 {
@@ -15,15 +18,27 @@ public class DemoHM
 	System.out.println(m.keySet());
 	System.out.println(m.entrySet());
 	System.out.println(m.containsKey(1));
-	System.out.println(m.remove(1));
+//	System.out.println(m.remove(1));
+	System.out.println();
 	
+//	Traversing method 1
+	System.out.println("Traversing method 1");
 	for(Map.Entry m1:m.entrySet())
 	{
-	System.out.print(m1.getKey());
-	System.out.print(" : "+m1.getValue());
-	System.out.println();
+	System.out.println(m1.getKey()+ " : "+m1.getValue());
 	}
+	System.out.println();
 	
+//	Traversing method 2 
+	System.out.println("Traversing method 2");
+	Set s = m.entrySet();
+	Iterator itr = s.iterator();
+	while(itr.hasNext())
+	{
+	    Map.Entry<Integer, String> kv = (Map.Entry<Integer, String>)itr.next();
+	    System.out.print("Key: "+kv.getKey());
+	    System.out.println(" Value: "+kv.getValue());
+	}
 	
     }
 }
